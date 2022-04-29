e2df69350901efc5619053b92759304a1fa8c288
AtaDogu <88247572+AtaDogu@users.noreply.github.com>
2022-04-29 23:05:15 +0300

    28/04-3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+
+const routes:Routes=[
+  {path:'',redirectTo:'/dashboard',pathMatch:'full'},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'movies',component:MoviesComponent}
+];
+
+@NgModule({
+  exports:[RouterModule],
+
+  imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule { }
--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,10 +1,10 @@
 import { Movie } from "./movie";
 
 export const Movies : Movie[]=[
-  {id:1,name:"Kal"},
-  {id:2,name:"Sal"},
-  {id:3,name:"Fal"},
-  {id:4,name:"Lal"},
-  {id:5,name:"Bal"},
-  {id:6,name:"Yal"},
+  {id:1,name:"Kal",description:"güzel",imageUrl:"1.jpeg"},
+  {id:2,name:"Sal",description:"kotu",imageUrl:"2.jpeg"},
+  {id:3,name:"Fal",description:"falan",imageUrl:"3.jpeg"},
+  {id:4,name:"Lal",description:"filan",imageUrl:"4.jpeg"},
+  {id:5,name:"Bal",description:"orta",imageUrl:"5.jpeg"},
+  {id:6,name:"Yal",description:"harika",imageUrl:"6.jpeg"},
 ]
